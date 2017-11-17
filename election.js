813fc033f21aa3995f80dc7b67a94347277c335f
Abby Christens <achristens@gmail.com>
2017-11-17 10:37:18 -0500

    Adding AJAX request with candidate name in data field

--- election.js
+++ election.js
@@ -9,20 +9,38 @@ document.addEventListener("DOMContentLoaded", function() {
     var array = responseData.candidates;
 
     array.forEach(function(each){
+      // Add each candidate name and votes:
       var candidateName  = each.name;
       var votes          = each.votes;
       var listItem       = document.createElement('li');
       listItem.className = 'candidate'
       listItem.innerText = candidateName + ": " + votes + " votes"
       list.append(listItem);
-    });
-
-    var candidates = document.querySelectorAll('.candidate');
-
-    candidates.forEach(function(each){
       var form = document.createElement('form');
-        each.append(form);
-    })
+
+      // Create form for each candidate to allow votes:
+      form.className = "candidate-form"
+      var hidden = document.createElement('input')
+      hidden.type  = "hidden"
+      hidden.name  = each.name
+      hidden.value = each.id
+      var submit = document.createElement('input')
+      submit.type = "submit"
+      form.append(hidden)
+      form.append(submit)
+      listItem.append(form);
+
+      // Add event listener for each form:
+      form.addEventListener('submit', function(e){
+        e.preventDefault();
+        $.ajax({
+          url: 'https://bb-election-api.herokuapp.com/vote',
+          method: 'POST',
+          data: {"name" : candidateName }
+        });
+      });
+    });
   });
+
 
 });
